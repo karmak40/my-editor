81b07ec75a5b6200b04971d646da225ca0f24f61
karmak40 <karmak40@outlook.com>
2021-11-27 22:35:31 +0100

    feature: persist data

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,6 +1,10 @@
-import { app, BrowserWindow } from "electron";
+import { app, BrowserWindow, ipcMain } from "electron";
+import * as fs from 'fs';
+import * as path from "path";
 
-function createWindow () {
+const contentFile = path.join(app.getPath('userData'), 'content.html');
+
+function createWindow() {
     const mainWindow = new BrowserWindow({
         width: 800,
         height: 600,
@@ -12,6 +16,20 @@ function createWindow () {
     mainWindow.loadFile('index.html');
 }
 
-app.whenReady().then(()=> {
+app.whenReady().then(() => {
     createWindow();
 })
+
+
+ipcMain.handle('getContent', () => {
+    if (fs.existsSync(contentFile)) {
+        const result = fs.readFileSync(contentFile);
+        return result.toString();
+    }
+    return '';
+})
+
+
+ipcMain.handle('setContent', ({ }, content: string) => {
+    fs.writeFileSync(contentFile, content);
+});
